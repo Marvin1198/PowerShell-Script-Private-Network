ff7223de1a673c9e4fa01512167823f317b00e33
Marvin Khandubhai Patel <c0874592@mylambton.ca>
2024-02-05 16:03:15 -0500

    Initial commit

---
+++ PowerShell-Script-Network.ps1
@@ -0,0 +1,12 @@
+﻿# Get all network connections
+$networkConnections = Get-NetConnectionProfile
+
+# Loop through each network connection and set the profile to private
+foreach ($connection in $networkConnections) {
+    $connectionName = $connection.Name
+    Write-Host "Setting network profile for '$connectionName' to Private..."
+    
+    Set-NetConnectionProfile -InterfaceAlias $connection.InterfaceAlias -NetworkCategory Private
+
+    Write-Host "Network profile for '$connectionName' set to Private."
+}
